ee1b82f5aae23d44e34e00c064a682b96af48d1f
Sahana M <sahanaumamallappa@gmail.com>
2019-02-22 21:33:52 +0530

    Update classifiers.py
    This is a classifier on a small data set of Height, weight and shoe size to classify male and female. I have used different models from sklearn includes Decision tree Classifier, Random Forest Classifier, Kneighbors Classifier, Logistic Regression, Naive Bayes

--- classifiers.py
+++ classifiers.py
@@ -17,17 +17,24 @@ clf = clf.fit(X,Y)
 prediction = clf.predict([[190, 70, 43]])
 print(prediction)
 
+#Output - Male
+
 #Random Forest Classifier
 clf2 = RandomForestClassifier(n_estimators=2)
 clf2 = clf2.fit(X,Y)
 prediction = clf2.predict([[190, 70, 43]])
 print(prediction)
 
+#Output -Male
+
+
 #Kneighbors Classifier
 neigh = KNeighborsClassifier(n_neighbors=3)
 neigh.fit(X, Y)
 prediction = neigh.predict([[190, 70, 43]])
 print (prediction)
+
+#Output -Male
 
 #Logistic Regression
 clf3 = LogisticRegression()
@@ -36,8 +43,12 @@ predicfrom sknn.mlp import Classifier, Layer
         import numpy as nption = clf3.predict([[190,70,43]])
 print(prediction)
 
+#Output -Female
+
 #Naive Bayes
 clf4 = GaussianNB()
 clf4 = clf4.fit(X,Y)
 prediction = clf4.predict([[190,70,43]])
 print(prediction)
+
+#Output -Male
