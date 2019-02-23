6cab68108eb5c34cf3f3822bb426482150cbce33
Sahana M <sahanaumamallappa@gmail.com>
2019-02-23 11:29:28 +0530

    Update classifiers.py

--- classifiers/classifiers.py
+++ classifiers/classifiers.py
@@ -1,1 +1,55 @@
 
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+
+#[height, weight, shoe size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
+
+#Decision tree Classifier
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X,Y)
+prediction = clf.predict([[190, 70, 43]])
+print(prediction)
+
+#Output - Male
+
+#Random Forest Classifier
+clf2 = RandomForestClassifier(n_estimators=2)
+clf2 = clf2.fit(X,Y)
+prediction = clf2.predict([[190, 70, 43]])
+print(prediction)
+
+#Output -Male
+
+
+#Kneighbors Classifier
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X, Y)
+prediction = neigh.predict([[190, 70, 43]])
+print (prediction)
+
+#Output -Male
+
+#Logistic Regression
+clf3 = LogisticRegression()
+clf3.fit(X,Y)
+predicfrom sknn.mlp import Classifier, Layer
+        import numpy as nption = clf3.predict([[190,70,43]])
+print(prediction)
+
+#Output -Female
+
+#Naive Bayes
+clf4 = GaussianNB()
+clf4 = clf4.fit(X,Y)
+prediction = clf4.predict([[190,70,43]])
+print(prediction)
+
+#Output -Male
